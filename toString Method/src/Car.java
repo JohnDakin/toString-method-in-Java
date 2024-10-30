3c78bbf437cbd6567229c3ec141325c4b5ed916a
John Dakin <jdakin749@gmail.com>
2024-10-30 12:02:57 +0300

    Add files via upload

---
+++ toString Method/src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car {
+	
+	String make = "Ford";
+	String model = "Mustang";
+	int year = 2021;
+	String color = "red";
+	
+	
+	public String toString() {
+		
+		return  make + "\n"+model+"\n"+color+"\n"+year;
+	}
+	
+}
